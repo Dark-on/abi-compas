78f824d7bbcb720c7423aa949656dc91e6372aee
201dreames <201dreamers@gmail.com>
2020-02-29 20:53:06 +0200

    Revert "uploaded venv"
    This reverts commit fc2e59e4ef2f8e0fde9bf94af4d81d9617f8515f.

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
-from app import application
+from app import acompass
 from flask import render_template, flash, redirect, url_for
 # from app.forms import LoginForm
 
 
-@application.route('/')
-@application.route('/index')
+@acompass.route('/')
+@acompass.route('/index')
 def index():
 
     return render_template('index.html')
 
 
-@application.route('/spec-test')
+@acompass.route('/spec-test')
 def spec_test():
 
     return render_template('spec-test.html')
 
 
-@application.route('/filter-universities')
+@acompass.route('/filter-universities')
 def filter_universities():
 
     return render_template('filter-universities.html')
