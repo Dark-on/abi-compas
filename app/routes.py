fc2e59e4ef2f8e0fde9bf94af4d81d9617f8515f
201dreames <201dreamers@gmail.com>
2020-02-29 20:51:22 +0200

    uploaded venv

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
-from app import acompass
+from app import application
 from flask import render_template, flash, redirect, url_for
 # from app.forms import LoginForm
 
 
-@acompass.route('/')
-@acompass.route('/index')
+@application.route('/')
+@application.route('/index')
 def index():
 
     return render_template('index.html')
 
 
-@acompass.route('/spec-test')
+@application.route('/spec-test')
 def spec_test():
 
     return render_template('spec-test.html')
 
 
-@acompass.route('/filter-universities')
+@application.route('/filter-universities')
 def filter_universities():
 
     return render_template('filter-universities.html')
