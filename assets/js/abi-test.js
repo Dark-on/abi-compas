c80ec7059577c09db2ec2587cd1370da3571ef32
darkness8129 <yaroslaw460@gmail.com>
2020-03-05 21:24:38 +0200

    commented code

--- assets/js/abi-test.js
+++ assets/js/abi-test.js
@@ -1,24 +1,29 @@
 "use strict";
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', function(){ //loaded html but no styles and so on
 	let elemIndex = 0,
+	/* get the buttons from html */
 	prevBut = document.getElementById('prev-btn'),
 	nextBut = document.getElementById('next-btn'),
 	submitBtn = document.getElementById('submit-btn');
-	showElem(elemIndex); 
+	showElem(elemIndex); // show first elem
 
+	/* calls the function and increases the element index by 1 */
 	prevBut.onclick = function() {
 		showElem(elemIndex -=1 );
 	}
+	/* calls the function and decreases the element index by 1 */
 	nextBut.onclick = function() {
 		showElem(elemIndex +=1 );	
 	}
 
+	/* function that shows an element */
 	function showElem(n){
 		let elements = document.querySelectorAll('.abi-test__item');
 
 		if(n < 0) elemIndex = 0;
 		if(n > elements.length - 1) elemIndex = elements.length - 1;
 
+		/* style-changing conditions */
 		switch(n){
 			case 0:
 				prevBut.style.display = 'none';
@@ -37,9 +42,11 @@ window.addEventListener('DOMContentLoaded', function(){
 				submitBtn.style.display = 'none';
 		}
 
+		/* display:none all elements */
 		for (let i = 0; i < elements.length; i++ ){
 			elements[i].style.display = "none";
 		}
+		/* show elem with curent index */
 		elements[elemIndex].style.display = "block";	
 	}
 });
--- assets/js/change-theme.js
+++ assets/js/change-theme.js
@@ -1,8 +1,11 @@
 "use strict";
-window.addEventListener('load', function(){
+window.addEventListener('load', function(){ //browser loaded HTML and external resources
+	/* get the buttons from html */
 	let butt1 = document.getElementById('button-light'),
 	butt2 = document.getElementById('button-dark'),
 	head = document.querySelector('head');
+
+	/* add darktheme link */
 	butt2.onclick = function(){
 		butt2.classList.add("active");
 		butt1.classList.remove("active");
@@ -13,6 +16,8 @@ window.addEventListener('load', function(){
 		link.href = '../assets/css/dark-theme.css';
 		head.append(link);
 	}
+
+	/* delete darktheme link */
 	butt1.onclick = function(){
 		butt1.classList.add("active");
 		butt2.classList.remove("active");
