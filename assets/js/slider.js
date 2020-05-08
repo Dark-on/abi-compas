c1ec2b49bedb27523245e4c126d10c6ac7aabcc3
darkness8129 <yaroslaw460@gmail.com>
2020-05-08 20:28:28 +0300

    dots slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,9 +1,17 @@
 window.addEventListener('DOMContentLoaded', function () {
     let slides = document.querySelectorAll('.intro__item'),
         arrowNext = document.querySelector('.arrow-next'),
-        arrowPrev = document.querySelector('.arrow-prev');
+        arrowPrev = document.querySelector('.arrow-prev'),
+        dots = document.querySelectorAll('.dot');
 
     let slideIndex = 0;
+    for (let i = 0; i < dots.length; i++){
+        dots[i].onclick = function(){
+            slideIndex = i;
+            showSlide(slideIndex);
+        }
+    }
+
     arrowNext.onclick = () => showSlide(slideIndex +=1);
     arrowPrev.onclick = () => showSlide(slideIndex -=1);
     showSlide(slideIndex);
@@ -15,8 +23,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
         for (let i = 0; i < slides.length; i++) {
             slides[i].style.display = 'none';
+            dots[i].classList.remove("active");
         }
-
+        dots[index].classList.add("active");
         slides[index].style.display = 'flex';
     }
 
